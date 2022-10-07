9f663207bec97141c11a60ad197ceb6ea89daf7a
Yuki Takatsu <yukitakatsu6468@gmail.com>
2022-10-07 17:46:43 +0900

    第一回のプログラムを追加

--- NLabo-Swift-FoodtrackerApp/ViewController.swift
+++ NLabo-Swift-FoodtrackerApp/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Propertis
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTextField.delegate = self
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultButton(_ sendar: Any) {
+        mealNameLabel.text = "Default"
     }
 
 
 }
 
+
+// MARK: TextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keybord
+        textField.resignFirstResponder()
+        return true
+    }
+}
