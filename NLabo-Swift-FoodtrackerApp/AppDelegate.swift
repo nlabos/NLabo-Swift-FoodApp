e8d8e663ee378f7c12a4fe13cf77fbc5ad651c12
Yuki Takatsu <yukitakatsu6468@gmail.com>
2022-10-12 14:41:03 +0900

    AppDelegate

--- NLabo-Swift-FoodtrackerApp/AppDelegate.swift
+++ NLabo-Swift-FoodtrackerApp/AppDelegate.swift
@@ -2,14 +2,15 @@
 //  AppDelegate.swift
 //  NLabo-Swift-FoodtrackerApp
 //
-//  Created by 髙津悠樹 on 2022/10/04.
-//
+
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
